3e140e56db69946e13f5336db930674cbf4994d9
slbabin <>
2023-07-22 22:04:45 +0000

    Filter funtions

---
+++ filter.js
@@ -0,0 +1,23 @@
+const people = [
+    {
+        name: "Michael",
+        age: "23",
+    },
+    {
+        name: "Lianna",
+        age: "16",
+    },
+    {
+        name: "Paul",
+        age: "18",
+    },
+];
+
+const oldEnough = people.filter(person => person.age >= 21);
+
+console.log(oldEnough);
+
+console.log( "");
+
+const paul = people.filter(p => p.name == "Paul")[0];
+console.log(paul);
--- map.js
+++ map.js
@@ -51,3 +51,43 @@ const students = [
 
 const studentsWithIds = students.map(student => [student.name,student.id]);
 console.log(studentsWithIds);
+
+
+let students2 = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+// single line, used different variable names so as not to clash with those below.
+let [John, ...Rest] = students2.map(itm => [itm.name, itm.results]);
+console.log(John);
+console.log(Rest);
+
+console.log('\n');
+// Or create function first and use in map
+const mapper = (itm) => [itm.name, itm.results];
+let [john, ...rest] = students2.map(mapper);
+console.log(john);
+console.log(rest);
